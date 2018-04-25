df0c68128e746c643197f6566ef0d2d32d5655e8
Unknown <paul.paris@epitech.eu>
2018-04-25 14:43:15 +0200

    [FileParsing Class]
    transformed the file parsing process into a class.

--- emulator/conf_file_parse.py
+++ emulator/conf_file_parse.py
@@ -2,22 +2,26 @@
 
 import sys
 
+class FileParsing:
+	filename = ""
 
-def get_ascii_conf_file():
-	try:
-		with open("ascii_file.txt", "r") as file:
-			ascii_list = []
-			_str = ''
-			for line in file:
-				if line[:1] != '#' and _str[:1] == '#':
-					_list = line[:-1].split('|')
-					for i in range(len(_list)):
-						_list[i] = _list[i].split(',')
-					_list.insert(0,_str[1:])
-					ascii_list.append(_list)
-				_str = line[:-1]
-	except(OSError, IOError):
-		print("Could not find or could not open ascii_file.conf !")
-		exit(84)
-
-	return (ascii_list)
+	def __init__(self):
+		self.filename = "ascii_file.txt"
+	
+	def ParseAsciiList(self):
+		try:
+			with open(self.filename, "r") as file:
+				ascii_list = []
+				_str = ''
+				for line in file:
+					if line[:1] != '#' and _str[:1] == '#':
+						_list = line[:-1].split('|')
+						for i in range(len(_list)):
+							_list[i] = _list[i].split(',')
+						_list.insert(0,_str[1:])
+						ascii_list.append(_list)
+					_str = line[:-1]
+		except(OSError, IOError):
+			print("Could not find or could not open " + self.filename + " !")
+			exit(84)
+		return (ascii_list)
